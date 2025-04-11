4ee171e31dfd5799406aeb844873663a16f851b2
rehansharaf <rehansharaf2011@gmail.com>
2025-04-11 15:00:10 +0500

    Update AppTest.java
    Second edited line on main

--- src/test/java/org/test/test/AppTest.java
+++ src/test/java/org/test/test/AppTest.java
@@ -16,5 +16,6 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
         System.out.println("This is the added line");
+        System.out.println("One more line added 2");
     }
 }
