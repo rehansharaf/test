1d110b56dec767d52ff557aa7dcd4cec5ac715b5
rehansharaf <rehansharaf@192.168.1.14>
2025-04-11 14:57:10 +0500

    This is the first commit on develop branch

---
+++ src/test/java/org/test/test/AppTest2_Develop.java
@@ -0,0 +1,18 @@
+package org.test.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest2_Develop {
+
+   
+    @Test
+    public void testUntitled() {
+    	
+    	System.out.println("Hello this is the testapp2_Develop branch code");
+    }
+}
