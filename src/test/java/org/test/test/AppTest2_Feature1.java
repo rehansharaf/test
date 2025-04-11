956978134f9280e026bf9b63e30f4a3f433d0bb0
rehansharaf <rehansharaf@192.168.1.14>
2025-04-11 17:40:56 +0500

    Changed feature 1

---
+++ src/test/java/org/test/test/AppTest2_Feature1.java
@@ -0,0 +1,18 @@
+package org.test.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest2_Feature1 {
+
+   
+    @Test
+    public void testUntitled() {
+    	
+    	System.out.println("This is the first line i am adding in the feature file");
+    }
+}
