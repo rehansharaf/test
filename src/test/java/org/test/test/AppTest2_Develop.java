572d9b93d8bfaebd049736290d7b0634c9929646
rehansharaf <rehansharaf@192.168.1.14>
2025-04-11 17:33:29 +0500

    Done

--- src/test/java/org/test/test/AppTest2_Develop.java
+++ src/test/java/org/test/test/AppTest2_Develop.java
@@ -16,5 +16,6 @@ public class AppTest2_Develop {
     	System.out.println("Hello this is the testapp2_Develop branch code");
     	System.out.println("What i can do now i updated on apptest file now");
     	System.out.println("Done with the practice");
+    	System.out.println("Hello");
     }
 }
