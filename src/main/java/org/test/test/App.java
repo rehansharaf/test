3676fd5fb626e3328e0283eccfc2a7e18dddb156
rehansharaf <rehansharaf2011@gmail.com>
2025-04-14 18:48:33 +0500

    Update App.java

--- src/main/java/org/test/test/App.java
+++ src/main/java/org/test/test/App.java
@@ -6,5 +6,6 @@ package org.test.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.print("New Line added on Apr 14th 2025");
     }
 }
